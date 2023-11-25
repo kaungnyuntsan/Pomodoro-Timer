d73ee19bf906e8dd6926793b512610e8bf56b601
Kaung-Nyunt-San <71218665+Kaung-Nyunt-San@users.noreply.github.com>
2023-11-25 09:57:02 +0630

    first draft

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SectionList, StyleSheet, Text, View, Button } from "react-native";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
+  const [second, setSecond] = useState(1);
+  const [minute, setMinute] = useState(1);
+
+  useEffect(() => {
+    if (second >= 0) {
+      const intervalID = setInterval(() => setSecond(second - 1), 1000);
+      return () => clearInterval(intervalID);
+    } else if (minute > 0) {
+      setMinute(minute - 1);
+      setSecond(9);
+    }
+  }, [second, minute]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        {minute < 10 ? "0" + minute : minute} :{" "}
+        {second < 10 ? "0" + second : second}{" "}
+      </Text>
+      <Button title="add" onPress={() => console.log(second)} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
   },
 });
